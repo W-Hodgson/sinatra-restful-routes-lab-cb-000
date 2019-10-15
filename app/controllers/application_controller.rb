b3302727683868035d77f95e4d1fb6a78eb7afec
Will Hodgson <will.hodgson@current-rms.com>
2019-10-15 13:49:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id/edit' do
-    @recipe = Recipe.find_by(params[:id])
+    @recipe = Recipe.find_by(id: params[:id])
     erb :edit
   end
 
